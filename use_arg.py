a19b6075e9002bbd6f169994452af7a6eb41b185
elzup <guild0105@gmail.com>
2022-06-17 00:15:00 +0900

    refactor

--- use_arg.py
+++ use_arg.py
@@ -1,14 +1,14 @@
-import argparse
+from argparse import ArgumentParser
 
-parser = argparse.ArgumentParser()
+parser = ArgumentParser()
 
 parser.add_argument("name")
 parser.add_argument("msg")
-parser.add_argument("--repeat")
+parser.add_argument("--emoji")
 parser.add_argument("--sleep")
 args = parser.parse_args()
 
 print(args.name)
 print(args.msg)
-print(args.repeat)
+print(args.emoji)
 print(args.sleep)
